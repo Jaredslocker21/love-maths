6e0807174d4c44a566c497f323b540bb87a73ad4
Jaredslocker21 <jaredslocker21@gmail.com>
2022-05-17 14:16:05 +0000

    Add js functions

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
+
